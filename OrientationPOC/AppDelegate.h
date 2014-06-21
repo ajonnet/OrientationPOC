dd274346db8103bb46c11335f6e77170b7c2ae54
Amit Jain <amit@azoi.com>
2014-06-21 14:08:59 +0530

    Initial Commit

---
+++ OrientationPOC/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  OrientationPOC
+//
+//  Created by Amit Jain on 21/06/14.
+//  Copyright (c) 2014 Amit Jain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ OrientationPOC/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  OrientationPOC
+//
+//  Created by Amit Jain on 21/06/14.
+//  Copyright (c) 2014 Amit Jain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
